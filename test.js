43e805740cc95f59e17f53a6fc15c3af69340bff
Calvin Metcalf <cmetcalf@appgeo.com>
2014-03-03 11:50:33 -0500

    dev dependencies, no underscore

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
-var should = require('should')
+var should = require('chai').should()
 var fs = require('fs')
 var extractor = require('./index.js')
-
+var fc = require("./FC.json")
 describe('arc-poly', function(){
-  it('should take a geometrycollection and return all polygons', function(){
-    var fc = JSON.parse(fs.readFileSync('./FC.geojson'))
+  it('should take a geometrycollection and return all polygons', function(done){
     res = extractor(fc)
     res.should.be.ok
-    fs.writeFileSync('./polys.geojson', JSON.stringify(res))
+    fs.writeFile('./polys.geojson', JSON.stringify(res), done)
   })
 })
