73ef38d89546791c0374afabafc20d839b9dd8c8
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-03-03 11:08:00 -0500

    working with polys and multipolys

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-module.exports = function(fc){
+var _ = require('lodash')
 
+module.exports = function(fc){
+  var polys = _(fc).cloneDeep()
+  polys.features = []
+  _(fc.features).each(function(feature){
+    if(feature.geometry.type === 'Polygon'){
+      polys.features.push(feature)
+    }
+    else if(feature.geometry.type === 'MultiPolygon'){
+      _(feature.geometry.coordinates).each(function(polyCoords){
+        var poly = _(polyTemplate).cloneDeep()
+        poly.geometry.coordinates = polyCoords
+        polys.features.push(poly)
+      })
+    }
+    else if(feature.geometry.type === 'GeometryCollection'){
+      _(feature.geometries).each(function(geometry){
+
+      })
+    }
+  })
+
+  return polys
+}
+
+var polyTemplate = {
+  type: 'Feature',
+  properties: {},
+  geometry: {
+    type: 'Polygon',
+    coordinates: []
+  }
 }
--- test.js
+++ test.js
@@ -1,7 +1,12 @@
 var should = require('should')
+var fs = require('fs')
+var extractor = require('./index.js')
 
 describe('arc-poly', function(){
   it('should take a geometrycollection and return all polygons', function(){
-
+    var fc = JSON.parse(fs.readFileSync('./FC.geojson'))
+    res = extractor(fc)
+    res.should.be.ok
+    fs.writeFileSync('./polys.geojson', JSON.stringify(res))
   })
 })
